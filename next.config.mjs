6ff697f3dbe7f5704e94bf89678998ba16c46221
Kadyl Bakyt <k.bakyt@recycle.kz>
2024-05-22 09:59:12 +0500

    added sharp

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
-import createNextIntlPlugin from 'next-intl/plugin';
- 
-const withNextIntl = createNextIntlPlugin();
- 
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
- 
+import nextIntl from "next-intl/plugin";
+
+// Your existing Next.js configuration
+const nextConfig = {
+  reactStrictMode: true,
+};
+
+// Use nextIntl to extend your configuration with the next-intl plugin
+const withNextIntl = nextIntl("./src/i18n.ts");
+
 export default withNextIntl(nextConfig);
